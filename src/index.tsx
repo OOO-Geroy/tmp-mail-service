2eab0bca2e52591481c2f7f30077bc8c14a0aaa3
Delave-las-Kure <t1evgeny93@gmail.com>
2022-06-18 20:05:23 +0300

    change root

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app';
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById('mail-tmp-root');
 const root = createRoot(rootElement!);
 
 root.render(
